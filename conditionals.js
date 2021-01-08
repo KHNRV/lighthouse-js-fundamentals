0b413e78b1659271953a44ed328e561559167a93
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-01-08 20:38:03 +0000

    minor fix

--- conditionals.js
+++ conditionals.js
@@ -1,9 +1,17 @@
+//###################################################
+//Weather Assistant Example
+//###################################################
+
 const raining = true;
 //const cold = true;
 const temperature = 30;
 
 if (raining) {
   console.log("Don't forget your umbrella!");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
 }
 
 if (temperature < 0) {
@@ -14,6 +22,10 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
 /* if (cold) {
   console.log("Make sure you pick out a scarf!");
 } else {
@@ -22,4 +34,14 @@ if (temperature < 0) {
 
 console.log("Now you're ready to go outside!");
 
-#############
+//###################################################
+//Voting right example
+//###################################################
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+//###################################################
